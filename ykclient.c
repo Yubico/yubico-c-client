c4048a073bf5070df2d705ba5ce568f92b79f2a4
Lee Hinman <lee.hinman.ctr@hpc.mil>
2012-04-27 13:50:34 -0500

    Fixed segfault caused by curl error
    - libcurl error were causing ykclient_request to free resources before
      they had been allocated
    - also added a fprintf to stderr to print out the curl error, the
      generic one did not provide enough info for someone to debug

--- ykclient.c
+++ ykclient.c
@@ -455,6 +455,7 @@ ykclient_request (ykclient_t * ykc, const char *yubikey)
   char *user_agent = NULL;
   char *status;
   int out;
+  ykclient_server_response_t *serv_response = NULL;
 
   if (!url_template)
     url_template = "http://api.yubico.com/wsapi/2.0/verify?id=%d&otp=%s";
@@ -595,6 +596,7 @@ ykclient_request (ykclient_t * ykc, const char *yubikey)
 
   if (curl_ret != CURLE_OK)
     {
+      fprintf(stderr, "Error with curl: %s\n", curl_easy_strerror(curl_ret));
       out = YKCLIENT_CURL_PERFORM_ERROR;
       goto done;
     }
@@ -605,8 +607,7 @@ ykclient_request (ykclient_t * ykc, const char *yubikey)
       goto done;
     }
 
-  ykclient_server_response_t *serv_response =
-    ykclient_server_response_init ();
+  serv_response = ykclient_server_response_init ();
   if (serv_response == NULL)
     {
       out = YKCLIENT_PARSE_ERROR;
