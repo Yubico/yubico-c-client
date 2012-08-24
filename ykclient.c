57c725f0fe0f50c0b1f30c58140c8e6c276856dc
Clemens Lang <neverpanic@gmail.com>
2012-08-24 23:44:26 +0200

    Fix clang warning for tautologic compare
    size_t is defined to be unsigned, thus size_t < 0 is a tautologic
    compare.

--- ykclient.c
+++ ykclient.c
@@ -528,7 +528,7 @@ ykclient_request (ykclient_t * ykc, const char *yubikey)
       if (!url)
 	return YKCLIENT_OUT_OF_MEMORY;
       wrote = snprintf (url, len, url_templates[i], ykc->client_id, encoded_otp);
-      if (wrote < 0 || wrote > len)
+      if (wrote > len)
 	return YKCLIENT_FORMAT_ERROR;
     }
 
