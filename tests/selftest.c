09611118933315ad735267cdd0f098588503c55c
Klas Lindfors <klas@yubico.com>
2012-11-07 09:51:29 +0100

    add test cases for hmac-sha1 from rfc 2202

--- tests/selftest.c
+++ tests/selftest.c
@@ -39,6 +39,8 @@
 #include "b64/cencode.h"
 #include "b64/cdecode.h"
 
+#include "rfc4634/sha.h"
+
 #define TEST(xX) printf ("\nTest %s:%d (%s): ", __FILE__, __LINE__, __FUNCTION__); \
   printf xX; \
   printf ("\n")
@@ -117,6 +119,85 @@ test_base64 (void)
   ret = strcmp(buf, "blahonga");
   assert(ret == 0);
 }
+
+/* test cases for HMAC-SHA1 from rcs 2202 */
+void
+test_hmac (void)
+{
+  int res;
+  uint8_t result[USHAMaxHashSize];
+
+  unsigned char text1[] = "Hi There";
+  unsigned char key1[20];
+  memset(key1, 0x0b, 20);
+  uint8_t expected1[] = {0xb6, 0x17, 0x31, 0x86, 0x55, 0x05, 0x72, 0x64, 0xe2, 0x8b, 0xc0, 0xb6, 0xfb, 0x37, 0x8c, 0x8e, 0xf1, 0x46, 0xbe, 0x00};
+
+  unsigned char text2[] = "what do ya want for nothing?";
+  unsigned char key2[] = "Jefe";
+  uint8_t expected2[] = {0xef, 0xfc, 0xdf, 0x6a, 0xe5, 0xeb, 0x2f, 0xa2, 0xd2, 0x74, 0x16, 0xd5, 0xf1, 0x84, 0xdf, 0x9c, 0x25, 0x9a, 0x7c, 0x79};
+
+  unsigned char text3[50];
+  memset(text3, 0xdd, 50);
+  unsigned char key3[20];
+  memset(key3, 0xaa, 20);
+  uint8_t expected3[] = {0x12, 0x5d, 0x73, 0x42, 0xb9, 0xac, 0x11, 0xcd, 0x91, 0xa3, 0x9a, 0xf4, 0x8a, 0xa1, 0x7b, 0x4f, 0x63, 0xf1, 0x75, 0xd3};
+
+  unsigned char text4[50];
+  memset(text4, 0xcd, 50);
+  unsigned char key4[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19};
+  uint8_t expected4[] = {0x4c, 0x90, 0x07, 0xf4, 0x02, 0x62, 0x50, 0xc6, 0xbc, 0x84, 0x14, 0xf9, 0xbf, 0x50, 0xc8, 0x6c, 0x2d, 0x72, 0x35, 0xda};
+
+  unsigned char text5[] = "Test With Truncation";
+  unsigned char key5[20];
+  memset(key5, 0x0c, 20);
+  uint8_t expected5[] = {0x4c, 0x1a, 0x03, 0x42, 0x4b, 0x55, 0xe0, 0x7f, 0xe7, 0xf2, 0x7b, 0xe1, 0xd5, 0x8b, 0xb9, 0x32, 0x4a, 0x9a, 0x5a, 0x04};
+
+  unsigned char text6[] = "Test Using Larger Than Block-Size Key - Hash Key First";
+  unsigned char key6[80];
+  memset(key6, 0xaa, 80);
+  uint8_t expected6[] = {0xaa, 0x4a, 0xe5, 0xe1, 0x52, 0x72, 0xd0, 0x0e, 0x95, 0x70, 0x56, 0x37, 0xce, 0x8a, 0x3b, 0x55, 0xed, 0x40, 0x21, 0x12};
+
+  unsigned char text7[] = "Test Using Larger Than Block-Size Key and Larger Than One Block-Size Data";
+  unsigned char key7[80];
+  memset(key7, 0xaa, 80);
+  uint8_t expected7[] = {0xe8, 0xe9, 0x9d, 0x0f, 0x45, 0x23, 0x7d, 0x78, 0x6d, 0x6b, 0xba, 0xa7, 0x96, 0x5c, 0x78, 0x08, 0xbb, 0xff, 0x1a, 0x91};
+
+  TEST(("HMAC-SHA1 case 1"));
+  res = hmac (SHA1, text1, 8, key1, 20, result);
+  assert(res == 0);
+  assert(memcmp(result, expected1, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 2"));
+  res = hmac (SHA1, text2, 28, key2, 4, result);
+  assert(res == 0);
+  assert(memcmp(result, expected2, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 3"));
+  res = hmac (SHA1, text3, 50, key3, 20, result);
+  assert(res == 0);
+  assert(memcmp(result, expected3, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 4"));
+  res = hmac (SHA1, text4, 50, key4, 25, result);
+  assert(res == 0);
+  assert(memcmp(result, expected4, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 5"));
+  res = hmac (SHA1, text5, 20, key5, 20, result);
+  assert(res == 0);
+  assert(memcmp(result, expected5, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 6"));
+  res = hmac (SHA1, text6, 54, key6, 80, result);
+  assert(res == 0);
+  assert(memcmp(result, expected6, 20) == 0);
+
+  TEST(("HMAC-SHA1 case 7"));
+  res = hmac (SHA1, text7, 73, key7, 80, result);
+  assert(res == 0);
+  assert(memcmp(result, expected7, 20) == 0);
+}
+
 
 int
 main (void)
@@ -337,6 +418,8 @@ main (void)
   test_v1_validation(client_id, client_b64key);
 
   test_base64();
+
+  test_hmac();
 
   printf ("All tests passed\n");
 
