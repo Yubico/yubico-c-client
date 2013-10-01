053b851accdfb7ed11e89ec1d99f8813b791df44
Klas Lindfors <klas@yubico.com>
2013-10-01 15:44:59 +0200

    add support for "new" url templates
    without the query string

--- ykclient.c
+++ ykclient.c
@@ -48,11 +48,19 @@
 #define NONCE_LEN 32
 #define MAX_TEMPLATES 255
 
+#define ADD_NONCE "&nonce="
+#define ADD_OTP "&otp="
+#define ADD_ID "?id="
+
+#define TEMPLATE_FORMAT_OLD 1
+#define TEMPLATE_FORMAT_NEW 2
+
 struct ykclient_st
 {
   const char *ca_path;
   size_t num_templates;
   char **url_templates;
+  int template_format;
   char last_url[256];
   unsigned int client_id;
   size_t keylen;
@@ -80,11 +88,11 @@ struct ykclient_handle_st
 };
 
 const char *default_url_templates[] = {
-  "http://api.yubico.com/wsapi/2.0/verify?id=%d&otp=%s",
-  "http://api2.yubico.com/wsapi/2.0/verify?id=%d&otp=%s",
-  "http://api3.yubico.com/wsapi/2.0/verify?id=%d&otp=%s",
-  "http://api4.yubico.com/wsapi/2.0/verify?id=%d&otp=%s",
-  "http://api5.yubico.com/wsapi/2.0/verify?id=%d&otp=%s",
+  "http://api.yubico.com/wsapi/2.0/verify",
+  "http://api2.yubico.com/wsapi/2.0/verify",
+  "http://api3.yubico.com/wsapi/2.0/verify",
+  "http://api4.yubico.com/wsapi/2.0/verify",
+  "http://api5.yubico.com/wsapi/2.0/verify",
 };
 
 const size_t default_num_templates = 5;
@@ -168,9 +176,9 @@ ykclient_init (ykclient_t ** ykc)
   /*
    * Set the default URLs (these can be overridden later)
    */
-  ykclient_set_url_templates (p,
-			      sizeof (default_url_templates) /
-			      sizeof (char *), default_url_templates);
+  ykclient_set_url_bases (p,
+			  sizeof (default_url_templates) /
+			  sizeof (char *), default_url_templates);
 
   return YKCLIENT_OK;
 }
@@ -571,6 +579,18 @@ ykclient_rc
 ykclient_set_url_templates (ykclient_t * ykc, size_t num_templates,
 			    const char **url_templates)
 {
+  ykclient_rc ret = ykclient_set_url_bases(ykc, num_templates, url_templates);
+  if(ret == YKCLIENT_OK)
+    {
+      ykc->template_format = TEMPLATE_FORMAT_OLD;
+    }
+  return ret;
+}
+
+ykclient_rc
+ykclient_set_url_bases (ykclient_t * ykc, size_t num_templates,
+			    const char **url_templates)
+{
   size_t i;
   if (num_templates > MAX_TEMPLATES)
     {
@@ -606,6 +626,8 @@ ykclient_set_url_templates (ykclient_t * ykc, size_t num_templates,
 	  return YKCLIENT_OUT_OF_MEMORY;
 	}
     }
+
+  ykc->template_format = TEMPLATE_FORMAT_NEW;
   return YKCLIENT_OK;
 }
 
@@ -796,6 +818,37 @@ ykclient_generate_nonce (ykclient_t * ykc, char **nonce)
   return YKCLIENT_OK;
 }
 
+static ykclient_rc ykclient_expand_new_url(const char *template,
+    const char *encoded_otp, const char *nonce, int client_id,
+    char **url_exp)
+{
+  size_t len = strlen(template) + strlen(encoded_otp) + strlen(ADD_OTP) + strlen(ADD_ID) + 1;
+  len += snprintf(NULL, 0, "%d", client_id);
+
+  if(nonce)
+    {
+      len += strlen(nonce) + strlen(ADD_NONCE);
+    }
+
+  *url_exp = malloc(len);
+  if(!*url_exp)
+    {
+      return YKCLIENT_OUT_OF_MEMORY;
+    }
+
+  if(nonce)
+    {
+      snprintf(*url_exp, len, "%s" ADD_ID "%d" ADD_NONCE "%s" ADD_OTP "%s",
+	  template, client_id, nonce, encoded_otp);
+    }
+  else
+    {
+      snprintf(*url_exp, len, "%s" ADD_ID "%d" ADD_OTP "%s", template,
+	  client_id, encoded_otp);
+    }
+  return YKCLIENT_OK;
+}
+
 static ykclient_rc ykclient_expand_old_url(const char *template,
     const char *encoded_otp, const char *nonce, int client_id,
     char **url_exp)
@@ -826,7 +879,6 @@ static ykclient_rc ykclient_expand_old_url(const char *template,
       size_t len;
       ssize_t wrote;
 
-#define ADD_NONCE "&nonce="
       len =
         strlen (*url_exp) + strlen (ADD_NONCE) + strlen (nonce) +
         1;
@@ -845,7 +897,7 @@ static ykclient_rc ykclient_expand_old_url(const char *template,
        * Also, we assume that everyone will have at least one parameter ("id=")
        * before "otp" so there is no need to search for "?otp=".
        */
-      otp_offset = strstr (*url_exp, "&otp=");
+      otp_offset = strstr (*url_exp, ADD_OTP);
       if (otp_offset == NULL)
         {
           /* point at \0 at end of url in case there is no otp */
@@ -906,8 +958,17 @@ ykclient_expand_urls (ykclient_t * ykc, ykclient_handle_t * ykh,
 
   for (i = 0; i < ykc->num_templates; i++)
     {
-	     ykclient_rc res = ykclient_expand_old_url(ykc->url_templates[i],
-	         encoded_otp, nonce, ykc->client_id, &ykh->url_exp[i]);
+      ykclient_rc res;
+      if(ykc->template_format == TEMPLATE_FORMAT_OLD)
+        {
+	  res = ykclient_expand_old_url(ykc->url_templates[i],
+	      encoded_otp, nonce, ykc->client_id, &ykh->url_exp[i]);
+        }
+      else
+	{
+	  res = ykclient_expand_new_url(ykc->url_templates[i],
+	      encoded_otp, nonce, ykc->client_id, &ykh->url_exp[i]);
+	}
       if(res != YKCLIENT_OK)
         {
           out = res;
@@ -1383,7 +1444,14 @@ ykclient_verify_otp_v2 (ykclient_t * ykc_in,
 
   if (urlcount != 0 && *urls != 0)
     {
-      ykclient_set_url_templates (ykc, urlcount, urls);
+      if(strstr(urls[0], ADD_OTP "%s"))
+	{
+	  ykclient_set_url_templates (ykc, urlcount, urls);
+	}
+      else
+	{
+	  ykclient_set_url_bases (ykc, urlcount, urls);
+	}
     }
 
   if (api_key)
